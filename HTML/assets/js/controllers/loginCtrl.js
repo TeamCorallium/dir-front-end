98b918a8d6dd71c4465de9457567455d6262cb33
Alejandro <aravelojulan@gmail.com>
2017-09-24 23:21:30 -0700

    change in loginCtrl.js

--- HTML/assets/js/controllers/loginCtrl.js
+++ HTML/assets/js/controllers/loginCtrl.js
@@ -37,5 +37,6 @@ app.controller('LoginCtrl',["$scope", "RestService", "$state", "$rootScope",'$co
         $scope.logout = function () {
             $cookies.remove('sessionid');
             $rootScope.userdata.username = '';
+            $scope.connected = false;
         };
     }]);
