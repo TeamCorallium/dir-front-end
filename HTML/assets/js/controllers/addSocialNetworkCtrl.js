62afd223b35015e49f56934f8de4eb64044f0905
Alejandro <aravelojulan@gmail.com>
2017-09-30 10:59:46 -0700

    change in index.html, style.css and addSocialNetworkCtrl.js

--- HTML/assets/js/controllers/addSocialNetworkCtrl.js
+++ HTML/assets/js/controllers/addSocialNetworkCtrl.js
@@ -10,6 +10,7 @@ app.controller('AddSocialNetworkCtrl',["$rootScope", "$scope", "RestService", "$
 
         $scope.addSocialNetwork = function (name,url) {
             var type = '';
+
             if (name == 'Facebook'){
                 type = 'social-facebook';
             } else if (name == 'Twitter'){
@@ -20,7 +21,42 @@ app.controller('AddSocialNetworkCtrl',["$rootScope", "$scope", "RestService", "$
                 type = 'social-instagram';
             } else if (name == 'Reddit'){
                 type = 'social-reddit';
+            } else if (name == 'Google'){
+                type = 'social-google';
+            } else if (name == 'YouTube'){
+                type = 'social-youtube';
+            } else if (name == 'RSS'){
+                type = 'social-rss';
+            } else if (name == 'Behance'){
+                type = 'social-behance';
+            } else if (name == 'DropBox'){
+                type = 'social-dropbox';
+            } else if (name == 'GitHub'){
+                type = 'social-github';
+            } else if (name == 'Skype'){
+                type = 'social-skype';
+            } else if (name == 'Spotify'){
+                type = 'social-spotify';
+            } else if (name == 'StumbleUpon'){
+                type = 'social-stumbleupon';
+            } else if (name == 'Tumblr'){
+                type = 'social-tumblr';
+            } else if (name == 'Vimeo'){
+                type = 'social-vimeo';
+            } else if (name == 'WordPress'){
+                type = 'social-wordpress';
+            } else if (name == 'Xing'){
+                type = 'social-xing';
+            } else if (name == 'Yahoo'){
+                type = 'social-yahoo';
+            } else if (name == 'VK'){
+                type = 'social-vk';
+            } else if (name == 'FourSquare'){
+                type = 'social-foursquare';
+            } else if (name == 'Flickr'){
+                type = 'social-flickr';
             }
+
             RestService.addSocialNetwork(name,url,type);
         };
 
