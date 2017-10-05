79509110d14b9113173b81bf8c08d4413bf05dfc
Alejandro <aravelojulan@gmail.com>
2017-10-04 21:53:42 -0700

    change in restService.js

--- HTML/assets/js/services/restService.js
+++ HTML/assets/js/services/restService.js
@@ -78,7 +78,7 @@ app.factory('RestService', ['$rootScope','$http', '$q','$cookies', '$httpParamSe
                         str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
                     return str.join("&");
                 },
-                data: {'username': username, 'password': password, 'email': email, 'pin': pin}
+                data: {'username': username, 'password': password, 'email': email, 'pin': pin, 'firstname': "", 'lastname': ""}
             }).success(function (data) {
                 console.log(data);
                 if (data['response'] == 'ok') {
