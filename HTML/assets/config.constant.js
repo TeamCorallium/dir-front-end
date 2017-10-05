3b235e2188f4aaa1dbbfc398c263a580823d3a00
Alejandro <aravelojulan@gmail.com>
2017-10-05 01:15:53 -0700

    change in config.constant.js, config.router.js, index.html and editUserProfile.html

--- HTML/assets/config.constant.js
+++ HTML/assets/config.constant.js
@@ -15,8 +15,7 @@ app.constant('JS_REQUIRED',{
         'userProfileCtrl': 'assets/js/controllers/userProfileCtrl.js',
         'editUserProfileCtrl': 'assets/js/controllers/editUserProfileCtrl.js',
         'viewProfileCtrl': 'assets/js/controllers/viewProfileCtrl.js',
-        'homeCtrl': 'assets/js/controllers/homeCtrl.js',
-        'mainCtrl': 'assets/js/main.js'
+        'homeCtrl': 'assets/js/controllers/homeCtrl.js'
     },
     modules:[{
 
