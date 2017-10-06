c02caa9d8f2c1ff8694abe3ef579fb3f6f078b45
Alejandro <aravelojulan@gmail.com>
2017-10-06 00:22:43 -0700

    change in editUserProfile.html and editUserProfileCtrl.js

--- HTML/assets/js/controllers/editUserProfileCtrl.js
+++ HTML/assets/js/controllers/editUserProfileCtrl.js
@@ -10,10 +10,10 @@ app.controller('EditUserProfileCtrl',["$scope", "$stateParams", "RestService", "
         $scope.cropper.sourceImage = null;
         $scope.cropper.croppedImage   = null;
         $scope.bounds = {};
-        $scope.bounds.left = 0;
-        $scope.bounds.right = 0;
-        $scope.bounds.top = 0;
-        $scope.bounds.bottom = 0;
+        $scope.bounds.left = 160;
+        $scope.bounds.right = 160;
+        $scope.bounds.top = 160;
+        $scope.bounds.bottom = 160;
 
         $scope.user = {
             username: '',
