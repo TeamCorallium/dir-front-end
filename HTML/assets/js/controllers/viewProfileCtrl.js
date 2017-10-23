ef6194bfb28379dc4518aabb6318dc3ae2bbfb47
Alejandro <aravelojulian@gmail.com>
2017-10-24 00:05:49 +0800

    put notification ttl

--- HTML/assets/js/controllers/viewProfileCtrl.js
+++ HTML/assets/js/controllers/viewProfileCtrl.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-app.controller('ViewProfileCtrl',["$rootScope", "$scope", "$stateParams", "RestService", "$state", "$cookies", "$window",
+app.controller('ViewProfileCtrl', ["$rootScope", "$scope", "$stateParams", "RestService", "$state", "$cookies", "$window",
     function ($rootScope, $scope, $stateParams, RestService, $state, $cookies, $window) {
 
         $scope.user = {
@@ -28,71 +28,71 @@ app.controller('ViewProfileCtrl',["$rootScope", "$scope", "$stateParams", "RestS
         $scope.getUser = function (username) {
             RestService.fetchUserByUser(username)
                 .then(
-                    function (data) {
-                        data = data.results;
-                        if (data.length > 0){
-                            $scope.user.username = data[0].username;
-                            $scope.user.firstname = data[0].first_name;
-                            $scope.user.lastname = data[0].last_name;
-                            $scope.user.email = data[0].email;
+                function (data) {
+                    data = data.results;
+                    if (data.length > 0) {
+                        $scope.user.username = data[0].username;
+                        $scope.user.firstname = data[0].first_name;
+                        $scope.user.lastname = data[0].last_name;
+                        $scope.user.email = data[0].email;
 
-                            if (data[0].profiles.length > 0) {
-                                getProfile(data[0].profiles[0]);
-                            }
-
-                            getSnippets(data[0].username);
-                            getSocialNetworks(data[0].username);
-
-                        } else {
-                            $state.go('home');
-                            $('#myModal').modal('show');
+                        if (data[0].profiles.length > 0) {
+                            getProfile(data[0].profiles[0]);
                         }
-                    },
-                    function (errResponse) {
-                        console.log(errResponse);
+
+                        getSnippets(data[0].username);
+                        getSocialNetworks(data[0].username);
+
+                    } else {
+                        $state.go('home');
+                        $('#myModal').modal('show');
                     }
+                },
+                function (errResponse) {
+                    console.log(errResponse);
+                }
                 );
         };
 
         var getProfile = function (url) {
             RestService.fetchObjectByUrl(url)
                 .then(
-                    function (data) {
+                function (data) {
 
-                        if (data != undefined){
-                            $scope.user.info = data.info;
-                            if (data.avatar != '' && data.avatar != null){
-                                var avatarArray = data.avatar.split("/");
-                                $scope.user.avatar = RestService.imageDir+avatarArray[avatarArray.length-1];
-                            } else {
-                                $scope.user.avatar = 'assets/images/default-user.png';
-                            }
-                            $scope.user.id = data.id;
-                            $scope.user.score = data.score;
-                            $scope.user.rating = data.rating;
+                    if (data != undefined) {
+                        $scope.user.info = data.info;
+                        if (data.avatar != '' && data.avatar != null) {
+                            var avatarArray = data.avatar.split("/");
+                            $scope.user.avatar = RestService.imageDir + avatarArray[avatarArray.length - 1];
                         } else {
-                            $state.go('home');
-                            // throw toaster with message profile not found
+                            $scope.user.avatar = 'assets/images/default-user.png';
                         }
+                        $scope.user.id = data.id;
+                        $scope.user.score = data.score;
+                        $scope.user.rating = data.rating;
+                    } else {
+                        $state.go('home');
+                        // throw toaster with message profile not found
+                    }
+                },
+                function (errResponse) {
+                    console.log(errResponse);
+                    // throw toaster with message errResponse
+                }
+                );
+        };
+
+        var getTshirts = function (urls) {
+            for (var i = 0; i < urls.length; i++) {
+                RestService.fetchObjectByUrl(urls[i])
+                    .then(
+                    function (data) {
+                        $scope.user.tshirts.push(data);
                     },
                     function (errResponse) {
                         console.log(errResponse);
                         // throw toaster with message errResponse
                     }
-                );
-        };
-
-        var getTshirts = function (urls) {
-            for (var i=0; i<urls.length; i++){
-                RestService.fetchObjectByUrl(urls[i])
-                    .then(
-                        function (data) {
-                            $scope.user.tshirts.push(data);
-                        },
-                        function (errResponse) {
-                            console.log(errResponse);
-                            // throw toaster with message errResponse
-                        }
                     );
             }
         };
@@ -100,37 +100,37 @@ app.controller('ViewProfileCtrl',["$rootScope", "$scope", "$stateParams", "RestS
         var getSnippets = function (username) {
             RestService.fetchSnippets(username)
                 .then(
-                    function (data) {
-                        for (var i=0; i<data.length; i++){
-                            data[i].body = replaceURLWithHTMLLinks(data[i].body);
-                            $scope.user.snippets.push(data[i]);
-                        }                        
-                    },
-                    function (errResponse) {
-                        console.log(errResponse);
-                        // throw toaster with message errResponse
+                function (data) {
+                    for (var i = 0; i < data.length; i++) {
+                        data[i].body = replaceURLWithHTMLLinks(data[i].body);
+                        $scope.user.snippets.push(data[i]);
                     }
+                },
+                function (errResponse) {
+                    console.log(errResponse);
+                    // throw toaster with message errResponse
+                }
                 );
         };
 
         var getSocialNetworks = function (username) {
             RestService.fetchSocialNetworks(username)
                 .then(
-                    function (data) {
-                        for (var i=0; i<data.length; i++){
-                            $scope.user.socialnetworks.push(data[i]);
-                        }                        
-                    },
-                    function (errResponse) {
-                        console.log(errResponse);
-                        // throw toaster with message errResponse
+                function (data) {
+                    for (var i = 0; i < data.length; i++) {
+                        $scope.user.socialnetworks.push(data[i]);
                     }
+                },
+                function (errResponse) {
+                    console.log(errResponse);
+                    // throw toaster with message errResponse
+                }
                 );
         };
 
-        var replaceURLWithHTMLLinks = function(text) {
+        var replaceURLWithHTMLLinks = function (text) {
             var re = /(\(.*?)?\b((?:https?|ftp|file):\/\/[-a-z0-9+&@#\/%?=~_()|!:,.;]*[-a-z0-9+&@#\/%=~_()|])/ig;
-            return text.replace(re, function(match, lParens, url) {
+            return text.replace(re, function (match, lParens, url) {
                 var rParens = '';
                 lParens = lParens || '';
 
@@ -147,7 +147,7 @@ app.controller('ViewProfileCtrl',["$rootScope", "$scope", "$stateParams", "RestS
                     //     /(.*)(\.?\).*)/.exec(url)
                     // because if (.*) is greedy then \.? never gets a chance.
                     if (m = /(.*)(\.\).*)/.exec(url) ||
-                            /(.*)(\).*)/.exec(url)) {
+                        /(.*)(\).*)/.exec(url)) {
                         url = m[1];
                         rParens = m[2] + rParens;
                     }
@@ -157,7 +157,7 @@ app.controller('ViewProfileCtrl',["$rootScope", "$scope", "$stateParams", "RestS
         };
 
         $scope.openModalSnippets = function () {
-            if($cookies.get('sessionid') != undefined){
+            if ($cookies.get('sessionid') != undefined) {
                 $('#modalSnippets').modal('show');
             } else {
                 $('#myModal').modal('show');
@@ -165,7 +165,7 @@ app.controller('ViewProfileCtrl',["$rootScope", "$scope", "$stateParams", "RestS
         };
 
         $scope.openAddSocialNetworkModal = function () {
-            if($cookies.get('sessionid') != undefined){
+            if ($cookies.get('sessionid') != undefined) {
                 $('#modalSocialNetwork').modal('show');
             } else {
                 $('#myModal').modal('show');
@@ -179,28 +179,28 @@ app.controller('ViewProfileCtrl',["$rootScope", "$scope", "$stateParams", "RestS
         $scope.getPopularUsers = function () {
             RestService.fetchObjectByUrl(RestService.profileDir)
                 .then(
-                    function (data) {
-                        $scope.users = data;
-                    },
-                    function (errResponse) {
-                        console.log(errResponse);
-                        // throw toaster with message errResponse
-                    }
+                function (data) {
+                    $scope.users = data;
+                },
+                function (errResponse) {
+                    console.log(errResponse);
+                    // throw toaster with message errResponse
+                }
                 );
         };
 
         $scope.getPopularUsers();
 
         $scope.goToProfile = function (owner) {
-            $cookies.put('exploreUser',owner,{path: '/'});
+            $cookies.put('exploreUser', owner, { path: '/' });
             $scope.getTshirt();
         };
 
         $scope.getAvatar = function (avatar) {
             var dirAvatar = '';
-            if (avatar != '' && avatar != null){
+            if (avatar != '' && avatar != null) {
                 var avatarArray = avatar.split("/");
-                dirAvatar = RestService.imageDir + avatarArray[avatarArray.length-1];
+                dirAvatar = RestService.imageDir + avatarArray[avatarArray.length - 1];
             } else {
                 dirAvatar = 'assets/images/default-user.png';
             }
@@ -210,7 +210,7 @@ app.controller('ViewProfileCtrl',["$rootScope", "$scope", "$stateParams", "RestS
 
         $scope.getTshirt = function () {
 
-            $scope.user.username =  '';
+            $scope.user.username = '';
             $scope.user.firstname = '';
             $scope.user.lastname = '';
             $scope.user.email = '';
@@ -223,25 +223,25 @@ app.controller('ViewProfileCtrl',["$rootScope", "$scope", "$stateParams", "RestS
             $scope.user.tshirts = [];
             $scope.user.snippets = [];
 
-            if($cookies.get('exploreUser')) {
+            if ($cookies.get('exploreUser')) {
                 $scope.getUser($cookies.get('exploreUser'));
-                $cookies.remove("exploreUser",{path: '/'});
-            } else {                
+                $cookies.remove("exploreUser", { path: '/' });
+            } else {
                 RestService.fetchTshirt($stateParams.id)
                     .then(
-                        function (data) {
-                            if (data.length > 0){
-                                $scope.getUser(data[0].owner);
-                            } else {
-                                $state.go('home');
-                                $('#myModal').modal('show');
-                            }
-                        },
-                        function (errResponse) {
-                            console.log(errResponse);
-                            // throw toaster with message errResponse
+                    function (data) {
+                        if (data.length > 0) {
+                            $scope.getUser(data[0].owner);
+                        } else {
+                            $state.go('home');
+                            $('#myModal').modal('show');
                         }
-                    );                    
+                    },
+                    function (errResponse) {
+                        console.log(errResponse);
+                        // throw toaster with message errResponse
+                    }
+                    );
             }
         };
 
