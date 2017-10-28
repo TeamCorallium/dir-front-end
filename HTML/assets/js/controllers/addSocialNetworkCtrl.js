54c2065053bf6621a4967c3672825716456ec3dc
Alejandro <aravelojulian@gmail.com>
2017-10-28 15:16:13 +0800

    modified add social network in edit profile

--- HTML/assets/js/controllers/addSocialNetworkCtrl.js
+++ HTML/assets/js/controllers/addSocialNetworkCtrl.js
@@ -241,8 +241,8 @@ app.controller('AddSocialNetworkCtrl', ["$rootScope", "$scope", "RestService", "
         $scope.isConnected = false;
 
         $scope.isFacebookConnected = function () {
-            console.log(FB + " FB");
-            console.dir(FB + " FB");
+            console.log(FB != 'undefined');
+            console.log(FB != null);
             FB.getLoginStatus(function (response) {
                 if (response.status === 'connected') {
                     $scope.isConnected = true;
