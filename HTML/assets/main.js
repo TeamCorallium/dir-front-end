b2c0e0f577955701801c40cd8768dab45ad3db68
Alejandro <aravelojulian@gmail.com>
2017-11-01 23:05:26 +0800

    adding translate module

--- HTML/assets/main.js
+++ HTML/assets/main.js
@@ -122,6 +122,29 @@ app.config(['growlProvider', function(growlProvider) {
     growlProvider.globalDisableCountDown(true);
 }]);
 
+// translate config
+app.config(['$translateProvider',
+function ($translateProvider) {
+
+    // prefix and suffix information  is required to specify a pattern
+    // You can simply use the static-files loader with this pattern:
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'assets/i18n/',
+        suffix: '.json'
+    });
+
+    // Since you've now registered more then one translation table, angular-translate has to know which one to use.
+    // This is where preferredLanguage(langKey) comes in.
+    $translateProvider.preferredLanguage('en');
+
+    // Store the language in the local storage
+    $translateProvider.useLocalStorage();
+
+    // Enable sanitize
+    $translateProvider.useSanitizeValueStrategy('sanitize');
+
+}]);
+
 window.fbAsyncInit = function() {
 FB.init({
   appId            : '128874921166794',
