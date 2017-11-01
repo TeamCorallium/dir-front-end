c67302c1730cc9bac1eb6c67d464f967ae7b7d51
Alejandro <aravelojulian@gmail.com>
2017-11-01 10:35:47 +0800

    adding button translate to inbox

--- HTML/assets/js/controllers/contactUsCtrl.js
+++ HTML/assets/js/controllers/contactUsCtrl.js
@@ -38,6 +38,41 @@ app.controller('ContactUsCtrl', ["$scope", "$state", "$rootScope", "RestService"
         }
 
         $scope.sendMessage = function () {
-            console.log($scope.message.email + " " + $scope.message.subject + " " + $scope.message.body);
+            RestService.sendMessage($scope.message.email, 'admin', $scope.message.subject, $scope.message.body, false);
         };
+
+        $rootScope.$on('SendMessage', function (event, data) {
+            $scope.message.email = '';
+            $scope.message.subject = '';
+            $scope.message.body = '';
+            growl.success("Message sended correctly", { title: 'Send Message' });
+        });
+
+        $rootScope.$on('WrongMessage', function (event, data) {
+            growl.error("Error sending message, please try again", { title: 'Send Message' });
+        });
+
+        $rootScope.$on('forbidden', function (event, data) {
+            if (RestService.getCookie('csrftoken') == null) {
+                RestService.fetchObjectByUrl(RestService.loginNext)
+                    .then(
+                    function (data) {
+                        console.log('get get ' + RestService.getCookie('csrftoken'));
+                    },
+                    function (errResponse) {
+                        console.log(errResponse);
+                    }
+                    );
+
+            } else {
+                console.log(RestService.getCookie('csrftoken'));
+            }
+
+            growl.error("We detected some problems, please try again", { title: 'Logins Problems' });
+        });
+
+        $rootScope.$on('LoginNetworkConnectionError', function (event, data) {
+            growl.error("Server Not Found. Check your internet connection.", { title: 'Network Connection' });
+        });
+        
     }]);
