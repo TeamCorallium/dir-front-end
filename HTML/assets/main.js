306b2d68a8ea6b72178dfaaab49616ecb0540359
Alejandro <aravelojulian@gmail.com>
2017-11-06 13:31:48 +0800

    adding filter htmltoplaintext

--- HTML/assets/main.js
+++ HTML/assets/main.js
@@ -32,6 +32,20 @@ app.run(['$rootScope', '$cookies',
         });
     }]);
 
+app.filter("words", function () {
+    return function (input, words) {
+        if (isNaN(words)) return input;
+        if (words <= 0) return '';
+        if (input) {
+            var inputWords = input.split(/\s+/);
+            if (inputWords.length > words) {
+                input = inputWords.slice(0, words).join(' ') + '...';
+            }
+        }
+        return input;
+    };
+});
+
 app.filter('cut', function () {
     return function (value, wordwise, max, tail) {
         if (!value) return '';
