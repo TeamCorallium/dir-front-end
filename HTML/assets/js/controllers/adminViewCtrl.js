9812d9bad4ded8d76d1565685d7055fbe686c370
Alejandro <aravelojulian@gmail.com>
2017-11-07 00:18:01 +0800

    adding admin view

--- HTML/assets/js/controllers/adminViewCtrl.js
+++ HTML/assets/js/controllers/adminViewCtrl.js
@@ -6,6 +6,18 @@
 app.controller('AdminViewCtrl', ["$rootScope", "$scope", "$stateParams", "RestService", "$state", "$cookies", "$window", "growl",
     function ($rootScope, $scope, $stateParams, RestService, $state, $cookies, $window, growl) {
 
+        $scope.stuff  = {
+            color: '',
+            size: '',
+            code: '',
+            pin: ''
+        };
 
+        $scope.addStuff = function() {
+            console.log($scope.stuff.color);
+            console.log($scope.stuff.size);
+            console.log($scope.stuff.code);
+            console.log($scope.stuff.pin);
+        };
 
     }]);
