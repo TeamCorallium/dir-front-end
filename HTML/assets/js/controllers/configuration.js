93e22751100066cff5d740d94d6d589815f0b1c0
Alejandro Ravelo Julian <18109633867@qq.com>
2017-11-19 16:28:42 +0800

    adding social network pages

--- HTML/assets/js/controllers/configuration.js
+++ HTML/assets/js/controllers/configuration.js
@@ -7,6 +7,10 @@
 app.controller('ConfigurationsCtrl', ["$rootScope", "$scope", "$stateParams", "RestService", "$state", "$cookies", "$window", "growl", "SweetAlert", "$translate",
     function($rootScope, $scope, $stateParams, RestService, $state, $cookies, $window, growl, SweetAlert, $translate) {
 
+        $rootScope.OptionsEdit = false;
+        $cookies.remove("exploreUser", {
+            path: '/'
+        });
 
 
     }
