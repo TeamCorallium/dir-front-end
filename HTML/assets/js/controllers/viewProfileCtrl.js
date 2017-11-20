16aaca93eeac1efedbf7a19178605a9347283980
Alejandro Ravelo Julian <18109633867@qq.com>
2017-11-20 12:42:01 +0800

    notifications

--- HTML/assets/js/controllers/viewProfileCtrl.js
+++ HTML/assets/js/controllers/viewProfileCtrl.js
@@ -231,7 +231,6 @@ app.controller('ViewProfileCtrl', ["$rootScope", "$scope", "$stateParams", "Rest
                 .then(
                     function(data) {
                         for (var i = 0; i < data.length; i++) {
-                            data[i].url = replaceURLWithHTMLLinks(data[i].url);
                             $scope.user.socialnetworks.push(data[i]);
                         }
                     },
@@ -497,34 +496,6 @@ app.controller('ViewProfileCtrl', ["$rootScope", "$scope", "$stateParams", "Rest
 
         $scope.changeMiddle = function(num) {
             $scope.indexShowMiddle = num;
-        };
-
-        var replaceURLWithHTMLLinks = function(text) {
-            var re = /(\(.*?)?\b((?:https?|ftp|file):\/\/[-a-z0-9+&@#\/%?=~_()|!:,.;]*[-a-z0-9+&@#\/%=~_()|])/ig;
-            return text.replace(re, function(match, lParens, url) {
-                var rParens = '';
-                lParens = lParens || '';
-
-                // Try to strip the same number of right parens from url
-                // as there are left parens.  Here, lParenCounter must be
-                // a RegExp object.  You cannot use a literal
-                //     while (/\(/g.exec(lParens)) { ... }
-                // because an object is needed to store the lastIndex state.
-                var lParenCounter = /\(/g;
-                while (lParenCounter.exec(lParens)) {
-                    var m;
-                    // We want m[1] to be greedy, unless a period precedes the
-                    // right parenthesis.  These tests cannot be simplified as
-                    //     /(.*)(\.?\).*)/.exec(url)
-                    // because if (.*) is greedy then \.? never gets a chance.
-                    if (m = /(.*)(\.\).*)/.exec(url) ||
-                        /(.*)(\).*)/.exec(url)) {
-                        url = m[1];
-                        rParens = m[2] + rParens;
-                    }
-                }
-                return lParens + "<a href='" + url + "'>" + url + "</a>" + rParens;
-            });
         };
     }
 ]);
