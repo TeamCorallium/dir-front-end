e93cc2a37b690b723d2ac1a786a3d71688a55716
Alejandro Ravelo Julian <18109633867@qq.com>
2017-11-21 14:18:23 +0800

    configurations

--- HTML/assets/js/controllers/userProfileCtrl.js
+++ HTML/assets/js/controllers/userProfileCtrl.js
@@ -276,7 +276,8 @@ app.controller('UserProfileCtrl', ["$rootScope", "$scope", "$stateParams", "Rest
         $scope.getUser($cookies.get('username'));
 
         var o = RestService.fetchNotificationUnreaded();
-        console.log(o + " user_profile")
+        console.dir(o + " user_profile")
+        console.log(o.unreaded + " user_profile")
             // $rootScope.notificationCount = RestService.fetchNotificationUnreaded();
         $rootScope.notificationCount = o;
         console.log($rootScope.notificationCount + " user_profile")
