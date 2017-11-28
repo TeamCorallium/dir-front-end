33e03b7de650480d4b02a87798b95396384e0183
tony <tony850421@gmx.es>
2017-11-27 23:12:29 -0500

    adding https

--- HTML/assets/js/services/restService.js
+++ HTML/assets/js/services/restService.js
@@ -2,70 +2,65 @@
 
 app.factory('RestService', ['$rootScope', '$http', '$q', '$cookies', '$httpParamSerializer', '$state', function($rootScope, $http, $q, $cookies, $httpParamSerializer, $state) {
 
-    var tshirt = 'http://www.dir.com:8888/api/tshirts/';
-    var users = 'http://www.dir.com:8888/api/users/';
-    var profiles = 'http://www.dir.com:8888/api/profiles/';
-    var login = 'http://www.dir.com:8888/api/api-auth/login/';
-    var loginNext = 'http://www.dir.com:8888/api/api-auth/login/?next=/';
-    var register = 'http://www.dir.com:8888/api/api-auth/register/';
-    var snippets = 'http://www.dir.com:8888/api/snippets/';
-    var socialnetwork = 'http://www.dir.com:8888/api/socialnetworks/';
-    var imageDir = 'http://www.dir.com/images/';
-    var imageDownload = 'http://www.dir.com:8888/api/qrcode';
-    var updateWithOutImage = 'http://www.dir.com:8888/api/updateprofile';
-    var messages = 'http://www.dir.com:8888/api/messages/';
-    var sendMessages = 'http://www.dir.com:8888/api/send-message/';
-    var updatePassword = 'http://www.dir.com:8888/api/api-auth/update/';
-    var clapDir = 'http://www.dir.com:8888/api/clap-profile/';
-    var linkStuff = 'http://www.dir.com:8888/api/link-stuff/';
-    var stocks = 'http://www.dir.com:8888/api/stocks/';
-    var deleteUser = 'http://www.dir.com:8888/api/delete-user/';
-    var tracking = 'http://www.dir.com:8888/api/track/';
-    var followDir = 'http://www.dir.com:8888/api/follow/';
-    var followersDir = 'http://www.dir.com:8888/api/followers/';
-    var unfollowDir = 'http://www.dir.com:8888/api/unfollow/';
-    var notifications = 'http://www.dir.com:8888/api/notifications/';
-    var notificationsUnreaded = 'http://www.dir.com:8888/api/notifications-unreaded/';
+    // var tshirt = 'http://www.dir.com:8888/api/tshirts/';
+    // var users = 'http://www.dir.com:8888/api/users/';
+    // var profiles = 'http://www.dir.com:8888/api/profiles/';
+    // var login = 'http://www.dir.com:8888/api/api-auth/login/';
+    // var loginNext = 'http://www.dir.com:8888/api/api-auth/login/?next=/';
+    // var register = 'http://www.dir.com:8888/api/api-auth/register/';
+    // var snippets = 'http://www.dir.com:8888/api/snippets/';
+    // var socialnetwork = 'http://www.dir.com:8888/api/socialnetworks/';
+    // var imageDir = 'http://www.dir.com/images/';
+    // var imageDownload = 'http://www.dir.com:8888/api/qrcode';
+    // var updateWithOutImage = 'http://www.dir.com:8888/api/updateprofile';
+    // var messages = 'http://www.dir.com:8888/api/messages/';
+    // var sendMessages = 'http://www.dir.com:8888/api/send-message/';
+    // var updatePassword = 'http://www.dir.com:8888/api/api-auth/update/';
+    // var clapDir = 'http://www.dir.com:8888/api/clap-profile/';
+    // var linkStuff = 'http://www.dir.com:8888/api/link-stuff/';
+    // var stocks = 'http://www.dir.com:8888/api/stocks/';
+    // var deleteUser = 'http://www.dir.com:8888/api/delete-user/';
+    // var tracking = 'http://www.dir.com:8888/api/track/';
+    // var followDir = 'http://www.dir.com:8888/api/follow/';
+    // var followersDir = 'http://www.dir.com:8888/api/followers/';
+    // var unfollowDir = 'http://www.dir.com:8888/api/unfollow/';
+    // var notifications = 'http://www.dir.com:8888/api/notifications/';
+    // var notificationsUnreaded = 'http://www.dir.com:8888/api/notifications-unreaded/';
 
-    // var tshirt = 'http://www.dirstuff.com/server/api/tshirts/';
-    // var users = 'http://www.dirstuff.com/server/api/users/';
-    // var profiles = 'http://www.dirstuff.com/server/api/profiles/';
-    // var login = 'http://www.dirstuff.com/server/api/api-auth/login/';
-    // var loginNext = 'http://www.dirstuff.com/server/api/api-auth/login/?next=/';
-    // var register = 'http://www.dirstuff.com/server/api/api-auth/register/';
-    // var snippets = 'http://www.dirstuff.com/server/api/snippets/';
-    // var socialnetwork = 'http://www.dirstuff.com/server/api/socialnetworks/';
-    // var imageDir = 'http://www.dirstuff.com/images/';
-    // var imageDownload = 'http://www.dirstuff.com/server/api/qrcode';
-    // var updateWithOutImage = 'http://www.dirstuff.com/server/api/updateprofile';
-    // var messages = 'http://www.dirstuff.com/server/api/messages/';
-    // var sendMessages = 'http://www.dirstuff.com/server/api/send-message/';
-    // var updatePassword = 'http://www.dirstuff.com/server/api/api-auth/update/';
-    // var clapDir = 'http://www.dirstuff.com/server/api/clap-profile/';
-    // var linkStuff = 'http://www.dirstuff.com/server/api/link-stuff/';
-    // var stocks = 'http://www.dirstuff.com/server/api/stocks/';
-    // var deleteUser = 'http://www.dirstuff.com/server/api/delete-user/';
-    // var tracking = 'http://www.dirstuff.com/server/api/track/';
-    // var followDir = 'http://www.dirstuff.com/server/api/follow/';
-    // var followersDir = 'http://www.dirstuff.com/server/api/followers/';
-    // var unfollowDir = 'http://www.dirstuff.com/server/api/unfollow/';
-    // var notifications = 'http://www.dirstuff.com/server/api/notifications/';
-    // var notificationsUnreaded = 'http://www.dirstuff.com/server/api/notifications-unreaded/';
+    var tshirt = 'https://www.dirstuff.com/server/api/tshirts/';
+    var users = 'https://www.dirstuff.com/server/api/users/';
+    var profiles = 'https://www.dirstuff.com/server/api/profiles/';
+    var login = 'https://www.dirstuff.com/server/api/api-auth/login/';
+    var loginNext = 'https://www.dirstuff.com/server/api/api-auth/login/?next=/';
+    var register = 'https://www.dirstuff.com/server/api/api-auth/register/';
+    var snippets = 'https://www.dirstuff.com/server/api/snippets/';
+    var socialnetwork = 'https://www.dirstuff.com/server/api/socialnetworks/';
+    var imageDir = 'https://www.dirstuff.com/images/';
+    var imageDownload = 'https://www.dirstuff.com/server/api/qrcode';
+    var updateWithOutImage = 'https://www.dirstuff.com/server/api/updateprofile';
+    var messages = 'https://www.dirstuff.com/server/api/messages/';
+    var sendMessages = 'https://www.dirstuff.com/server/api/send-message/';
+    var updatePassword = 'https://www.dirstuff.com/server/api/api-auth/update/';
+    var clapDir = 'https://www.dirstuff.com/server/api/clap-profile/';
+    var linkStuff = 'https://www.dirstuff.com/server/api/link-stuff/';
+    var stocks = 'https://www.dirstuff.com/server/api/stocks/';
+    var deleteUser = 'https://www.dirstuff.com/server/api/delete-user/';
+    var tracking = 'https://www.dirstuff.com/server/api/track/';
+    var followDir = 'https://www.dirstuff.com/server/api/follow/';
+    var followersDir = 'https://www.dirstuff.com/server/api/followers/';
+    var unfollowDir = 'https://www.dirstuff.com/server/api/unfollow/';
+    var notifications = 'https://www.dirstuff.com/server/api/notifications/';
+    var notificationsUnreaded = 'https://www.dirstuff.com/server/api/notifications-unreaded/';
 
-    // var urlBase = '/server/api';
-    var urlBase = '/api';
+    var urlBase = '/server/api';
+    // var urlBase = '/api';
 
     return {
         loginNext: loginNext,
-
         imageDir: imageDir,
-
         usersDir: users,
-
         profileDir: profiles,
-
         messageDir: messages,
-
         urlBaseDir: urlBase,
 
         getCookie: function(name) {
