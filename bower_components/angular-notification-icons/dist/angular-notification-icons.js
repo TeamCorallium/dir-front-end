e1b669a86032c73d959a11af04fd9dc7e8d71f24
Alejandro Ravelo Julian <18109633867@qq.com>
2017-12-05 17:40:33 +0800

    notifications

--- bower_components/angular-notification-icons/dist/angular-notification-icons.js
+++ bower_components/angular-notification-icons/dist/angular-notification-icons.js
@@ -12,7 +12,7 @@
 angular.module('angular-notification-icons', ['angular-notification-icons.tpls']);
 angular.module('angular-notification-icons.tpls', []);
 
-angular.module("angular-notification-icons.tpls").run(["$templateCache", function($templateCache) {$templateCache.put("template/notification-icon.html","<div class=\"angular-notifications-container\">\n    <div class=\"angular-notifications-icon overlay\" ng-show=\"notification.visible\"><div ng-hide=\"notification.hideCount\">{{notification.count}}</div></div>\n    <div class=\"notification-inner\">\n        <ng-transclude></ng-transclude>\n    </div>\n</div>");}]);
+angular.module("angular-notification-icons.tpls").run(["$templateCache", function($templateCache) {$templateCache.put("template/notification-icon.html","<div class=\"angular-notifications-container\">\n    <div class=\"angular-notifications-icon overlay\" ng-show=\"notification.visible\"><div class=\"notificationMargin\" ng-hide=\"notification.hideCount\">{{notification.count}}</div></div>\n    <div class=\"notification-inner\">\n        <ng-transclude></ng-transclude>\n    </div>\n</div>");}]);
 /* global angular */
 
 'use strict';
