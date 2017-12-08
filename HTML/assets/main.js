c1790afea7bd184cf070d66ba998283adc9ea382
Alejandro Ravelo Julian <18109633867@qq.com>
2017-12-08 13:35:20 +0800

    fixing register

--- HTML/assets/main.js
+++ HTML/assets/main.js
@@ -101,10 +101,10 @@ app.directive('okPassword',[ function() {
                     var pwd = $scope.password = $element.val();
 
                     // resolve password strength score using zxcvbn service
-                    $scope.passwordStrength = pwd ? (pwd.length > 7) : null;
+                    // $scope.passwordStrength = pwd ? (pwd.length > 7) : null;
 
                     // define the validity criterion for okPassword constraint
-                    ngModelCtrl.$setValidity('okPassword', $scope.passwordStrength >= 2);
+                    ngModelCtrl.$setValidity('okPassword', pwd.length > 7);
                 });
             });
         }
