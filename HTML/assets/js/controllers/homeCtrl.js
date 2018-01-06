f3d3b9c3b726f8b4f2c1061ab52e7bea5bed12fc
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-01-06 18:35:17 +0800

    design changes

--- HTML/assets/js/controllers/homeCtrl.js
+++ HTML/assets/js/controllers/homeCtrl.js
@@ -28,12 +28,34 @@ app.controller('HomeCtrl', ["$scope", "$state", "$rootScope", "RestService", "$c
             $scope.countLimit = 3;
         }
 
+        $scope.countBackers = 4;
+
+        if ($(window).width() >= 992) {
+            $scope.countBackers = 4;
+        } else if ($(window).width() >= 768) {
+            $scope.countBackers = 3;
+        } else if ($(window).width() >= 480) {
+            $scope.countBackers = 2;
+        } else {
+            $scope.countBackers = 1;
+        }
+
         $(window).on("resize.doResize", function() {
             $scope.$apply(function() {
                 if ($(window).width() >= 992) {
                     $scope.countLimit = 4;
                 } else if ($(window).width() >= 768) {
                     $scope.countLimit = 3;
+                }
+
+                if ($(window).width() >= 992) {
+                    $scope.countBackers = 4;
+                } else if ($(window).width() >= 768) {
+                    $scope.countBackers = 3;
+                } else if ($(window).width() >= 480) {
+                    $scope.countBackers = 2;
+                } else {
+                    $scope.countBackers = 1;
                 }
             });
         });
@@ -157,32 +179,6 @@ app.controller('HomeCtrl', ["$scope", "$state", "$rootScope", "RestService", "$c
                 'HTML/assets/images/slider/slide3.jpg',
             ]
         };
-
-        $scope.countBackers = 4;
-
-        if ($(window).width() >= 992) {
-            $scope.countBackers = 4;
-        } else if ($(window).width() >= 768) {
-            $scope.countBackers = 3;
-        } else if ($(window).width() >= 480) {
-            $scope.countBackers = 2;
-        } else {
-            $scope.countBackers = 1;
-        }
-
-        $(window).on("resize.doResize", function() {
-            $scope.$apply(function() {
-                if ($(window).width() >= 992) {
-                    $scope.countBackers = 4;
-                } else if ($(window).width() >= 768) {
-                    $scope.countBackers = 3;
-                } else if ($(window).width() >= 480) {
-                    $scope.countBackers = 2;
-                } else {
-                    $scope.countBackers = 1;
-                }
-            });
-        });
 
         $scope.backers = {
             slides: [{
