d56ca62a00c7f4499fba3ae6073fb7801210d415
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-01-10 16:04:41 +0800

    adding change

--- HTML/assets/js/controllers/userProfileCtrl.js
+++ HTML/assets/js/controllers/userProfileCtrl.js
@@ -31,6 +31,8 @@ app.controller('UserProfileCtrl', ["$rootScope", "$scope", "$stateParams", "Rest
             configReceiveEmails: ''
         };
 
+        $scope.coverCopy = '';
+
         $scope.message = {
             username: '',
             user: '',
@@ -296,9 +298,8 @@ app.controller('UserProfileCtrl', ["$rootScope", "$scope", "$stateParams", "Rest
             var url = RestService.coverPicture + $scope.user.coverId + "/";
             url = url.replace("/api", RestService.urlBaseDir);
             if ($scope.user.cover instanceof File) {
-                var coverCopy = '';
                 angular.copy($scope.user.cover, coverCopy);
-                RestService.updateCover(url, coverCopy);
+                RestService.updateCover(url, $scope.user.cover);
             }
         };
 
@@ -1314,6 +1315,11 @@ app.controller('UserProfileCtrl', ["$rootScope", "$scope", "$stateParams", "Rest
             } else if ($scope.indexShowMiddle == 2) {
                 $scope.getFollowing();
             }
+        });
+
+        $rootScope.$on('updateCover', function(event, data) {
+            console.log($scope.user.cover + " " + $scope.coverCopy);
+            $scope.user.cover = $scope.coverCopy;
         });
     }
 ]);
