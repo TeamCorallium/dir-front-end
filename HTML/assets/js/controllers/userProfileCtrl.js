591433d9f33756a14eaeac698fd0d62efd1552c8
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-01-12 15:51:10 +0800

    adding change

--- HTML/assets/js/controllers/userProfileCtrl.js
+++ HTML/assets/js/controllers/userProfileCtrl.js
@@ -236,18 +236,9 @@ app.controller('UserProfileCtrl', ["$rootScope", "$scope", "$stateParams", "Rest
                 var rParens = '';
                 lParens = lParens || '';
 
-                // Try to strip the same number of right parens from url
-                // as there are left parens.  Here, lParenCounter must be
-                // a RegExp object.  You cannot use a literal
-                //     while (/\(/g.exec(lParens)) { ... }
-                // because an object is needed to store the lastIndex state.
                 var lParenCounter = /\(/g;
                 while (lParenCounter.exec(lParens)) {
                     var m;
-                    // We want m[1] to be greedy, unless a period precedes the
-                    // right parenthesis.  These tests cannot be simplified as
-                    //     /(.*)(\.?\).*)/.exec(url)
-                    // because if (.*) is greedy then \.? never gets a chance.
                     if (m = /(.*)(\.\).*)/.exec(url) ||
                         /(.*)(\).*)/.exec(url)) {
                         url = m[1];
