3cf59e3c12b41807996d99c6e403865c8f4cce6d
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2018-01-15 11:15:54 +0800

    fixing problems

--- GruntFile.js
+++ GruntFile.js
@@ -99,6 +99,8 @@ module.exports = function(grunt) {
                     'bower_components/magnific-popup/dist/magnific-popup.css',
                     'bower_components/sweetalert/dist/sweetalert.css',
                     'bower_components/ng-img-crop/compile/minified/ng-img-crop.css',
+                    'bower_components/slick-carousel/slick/slick.css',
+                    'bower_components/slick-carousel/slick/slick-theme.css',
                     'HTML/assets/css/*.css',
                 ],
                 dest: 'dist/css.css'
